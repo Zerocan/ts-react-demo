764f2c08a5f773928377d7185369ea7c52d90aa9
Zerocan <1285711756@qq.com>
2020-03-17 21:00:26 +0800

    copy files

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Hello from './components/demo/Hello';
-import HelloClass from './components/demo/HelloClass';
+// import HelloClass from './components/demo/HelloClass';
+// import HelloHOC from './components/demo/HelloHOC';
+// import HelloHooks from './components/demo/HelloHooks';
+import Root from './routers';
 
-ReactDOM.render(<HelloClass name="TypeScript" />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
